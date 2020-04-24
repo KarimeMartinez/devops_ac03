00318753707fcc00e93981a090260196a1749d1a
KarimeMartinez <61034288+KarimeMartinez@users.noreply.github.com>
2020-04-24 00:50:14 -0300

    Exer02

--- Exercicio_02.py
+++ Exercicio_02.py
@@ -3,7 +3,6 @@ conn = sqlite3.connect(':memory:')
 
 c = conn.cursor()
 
-
 c.execute('''CREATE TABLE exercicio1_karime(
                 id integer,
                 nome text not null,
@@ -11,24 +10,18 @@ c.execute('''CREATE TABLE exercicio1_karime(
                 primary key (id) )'''
                 )
 
-
 c.execute("INSERT INTO exercicio1_karime VALUES (1,'KARIME MARTINEZ','karime@1234.com')")
 
 c.execute('''SELECT * 
                 FROM sqlite_master AS m, pragma_table_info(m.name)
                 WHERE tbl_name = 'exercicio1_karime'
             ''')
-
-         
+      
 for row in c.fetchall():
     print('--------------- Tabelas  --------------- ')
     print('Nomes dos campos: ', row[6])
     print('É PK?: ', row[10])
     print('Permite nulo? ', row[8])
 
-
-
 conn.commit()
-
-
 conn.close()

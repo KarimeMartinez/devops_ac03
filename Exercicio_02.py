67bc72e11293365052f5a5862e66f197412d0c22
KarimeMartinez <61034288+KarimeMartinez@users.noreply.github.com>
2020-04-24 00:48:39 -0300

    Exercicio2 AC03

---
+++ Exercicio_02.py
@@ -0,0 +1,34 @@
+import sqlite3
+conn = sqlite3.connect(':memory:')
+
+c = conn.cursor()
+
+
+c.execute('''CREATE TABLE exercicio1_karime(
+                id integer,
+                nome text not null,
+                email text,
+                primary key (id) )'''
+                )
+
+
+c.execute("INSERT INTO exercicio1_karime VALUES (1,'KARIME MARTINEZ','karime@1234.com')")
+
+c.execute('''SELECT * 
+                FROM sqlite_master AS m, pragma_table_info(m.name)
+                WHERE tbl_name = 'exercicio1_karime'
+            ''')
+
+         
+for row in c.fetchall():
+    print('--------------- Tabelas  --------------- ')
+    print('Nomes dos campos: ', row[6])
+    print('É PK?: ', row[10])
+    print('Permite nulo? ', row[8])
+
+
+
+conn.commit()
+
+
+conn.close()
